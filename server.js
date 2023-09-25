363be3434f03386b7d8546092716f7e1c3d93ebb
Ucheizzzy <uchechukwuobiakor@gmail.com>
2023-09-25 17:12:10 +0100

    installed all the packages needed

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-import { value } from './test.js'
-import randomValue from './test.js'
-console.log(value, randomValue)
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`)
+})
