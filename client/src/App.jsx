5f702e79d47a23e9cd84f760e730d7fee5e9afbd
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-05 22:52:25 +0100

    working on the dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {
 } from './pages'
 
 import { action as registerAction } from './pages/Register'
+import { action as loginAction } from './pages/Login'
 
 export const checkDefaultTheme = () => {
   const preferredTheme = window.matchMedia(
@@ -43,6 +44,7 @@ const router = createBrowserRouter([
       {
         path: 'login',
         element: <Login />,
+        action: loginAction,
       },
       {
         path: 'dashboard',
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link, Form, redirect, useNavigation } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
 import { FormRow } from '../components'
+import customFetch from '../utils/customFetch'
+import { toast } from 'react-toastify'
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  try {
+    await customFetch.post('/auth/login', data)
+    toast.success('Login successful')
+    return redirect('/dashboard')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
+}
 const Login = () => {
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
   return (
     <Wrapper>
-      <form className='form'>
+      <Form className='form' method='POST'>
         <h1 className='logo'>JOBNOTE</h1>
         <h4>Login</h4>
 
         <FormRow type='email' name='email' defaultValue='uche@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret' />
 
-        <button className='btn btn-block' type='submit'>
-          Submit
+        <button className='btn btn-block' type='submit' disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <button className='btn btn-block' type='submit'>
           Explore the app
@@ -23,7 +40,7 @@ const Login = () => {
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   )
 }
