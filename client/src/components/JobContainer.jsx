23cbf89cfddf4df6e876aa8e0dde72b8d06356cb
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-06 22:04:50 +0100

    working on edit job

--- client/src/components/JobContainer.jsx
+++ client/src/components/JobContainer.jsx
@@ -1,9 +1,26 @@
+import Wrapper from '../assets/wrappers/JobsContainer'
 import { useAllJobContext } from '../pages/AllJobs'
+import Job from './Job'
 
 const JobContainer = () => {
   const { data } = useAllJobContext()
   const { jobs } = data
-  return <div>JobContainer</div>
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No job to display</h2>
+      </Wrapper>
+    )
+  }
+  return (
+    <Wrapper>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />
+        })}
+      </div>
+    </Wrapper>
+  )
 }
 
 export default JobContainer
