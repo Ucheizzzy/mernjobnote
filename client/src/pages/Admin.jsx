60696ecdc5be06fcecad86022da65177dbc1a41b
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-08 09:07:50 +0100

    working on statItem for the Admin user

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,4 +1,18 @@
+import { redirect, useLoaderData } from 'react-router-dom'
+import customFetch from '../utils/customFetch'
+import { toast } from 'react-toastify'
+import Wrapper from '../assets/wrappers/StatsContainer'
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/admin/app-stats')
+    return data
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return redirect('dashboard')
+  }
+}
 const Admin = () => {
-  return <div>Admin</div>
+  const { users, jobs } = useLoaderData()
+  return <Wrapper>Admin</Wrapper>
 }
 export default Admin
